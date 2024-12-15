95f67dc6190eaf2bf2e964d5b6726d86ba827a68
Denis <Oleborn@mail.ru>
2024-12-15 20:17:59 +0700

    Initial commit

---
+++ src/main/java/oleborn/cicdresrch/CicdResrchApplication.java
@@ -0,0 +1,13 @@
+package oleborn.cicdresrch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdResrchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdResrchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oleborn/cicdresrch/CicdResrchApplicationTests.java
@@ -0,0 +1,13 @@
+package oleborn.cicdresrch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdResrchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
