4c566792339330eb1f9ff108a2208e5473387945
Denis <Oleborn@mail.ru>
2024-12-15 21:28:48 +0700

    update

--- src/test/java/oleborn/cicdresrch/CicdResrchApplicationTests.java
+++ src/test/java/oleborn/cicdresrch/CicdResrchApplicationTests.java
@@ -1,13 +1,24 @@
 package oleborn.cicdresrch;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@WebMvcTest
 class CicdResrchApplicationTests {
 
+    @Resource
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes("Привет, а я работаю!".getBytes()));
     }
 
 }
